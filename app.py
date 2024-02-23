f37368bd31f5871b1914f130900b5e50411cf279
“Danny <da.jimenezm@uniandes.edu.co>
2024-02-23 09:55:44 -0500

    Formato cuatro imagenes con caption ok

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ import source as sc
 # Título centrado en la página
 st.markdown("<h1 style='text-align: center;'>Bienvenidos a la aplicación para clasificar café</h1>", unsafe_allow_html=True)
 
+
+
+
 # Cargar imagen y mostrarla en un solo clic
 uploaded_file = st.file_uploader("")
 path="imagen_user.jpg"
@@ -15,28 +18,36 @@ path="imagen_user.jpg"
 if uploaded_file is not None:
     
     col1, col2 = st.columns(2)
+    
     image = Image.open(uploaded_file)
-    image_user = image.save(path)    
+    image_user = image.save(path)  
     img_normal,ref_white,mean,sample = sc.Normal(sc.read_img(path),white_limit=240)
+    
+    
     col1.image(uploaded_file, use_column_width=True, output_format='auto')
+    col1.markdown("<p style='text-align: center; font-size: 18px; color: white; font-weight: bold; font-style: italic;'>Imagen del Usuario</p>", unsafe_allow_html=True)
+
     col2.image(img_normal, use_column_width=True, output_format='auto')
+    col2.markdown("<p style='text-align: center; font-size: 18px; color: white; font-weight: bold; font-style: italic;'>Imagen Procesada</p>", unsafe_allow_html=True)
+
     
     #st.image(img_normal, use_column_width=True, output_format='auto')
     #st.markdown("<p style='text-align: center", unsafe_allow_html=True)
-
-
 
     Lab = sc.test_unfold(img_normal)
     Mas_V, img_mas_V, Mas_P, img_mas_P, Mas_M, img_ma_M, Mas_SM, img_ma_SM = sc.test_mask(Lab, img_normal)
     porcentaje_suma, porcentaje_bueno, porcentaje_malo, img_bueno, img_malo = sc.test_result(Mas_V, img_mas_V, Mas_P, img_mas_P, Mas_M, img_ma_M, Mas_SM, img_ma_SM, sample)         
 
-
-    
-    #col1.image(img_bueno, caption="Imagen Izquierda", use_column_width=True, output_format='auto')
     col1.image(img_bueno.reshape(img_normal.shape), use_column_width=True, output_format='auto')
+    col1.markdown("<p style='text-align: center; font-size: 18px; color: white; font-weight: bold; font-style: italic;'>Café bueno</p>", unsafe_allow_html=True)
+
     col2.image(img_malo.reshape(img_normal.shape), use_column_width=True, output_format='auto')
+    col2.markdown("<p style='text-align: center; font-size: 18px; color: white; font-weight: bold; font-style: italic;'>Café malo</p>", unsafe_allow_html=True)
 
 
+   # Mostrar porcentajes
+    st.text(f"Porcentaje de Café Bueno: {porcentaje_bueno}%")
+    st.text(f"Porcentaje de Café Malo: {porcentaje_malo}%")
 
     
 
