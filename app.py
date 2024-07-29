9d41db98c52c7910aee2b030cd0c3bbee87e455a
Danny <da.jimenezm@uniandes.edu.co>
2024-07-29 16:26:20 -0500

    centrando radio en celular 3.4

--- app.py
+++ app.py
@@ -27,6 +27,18 @@ image_base64 = load_image(ruta_logo)
 st.markdown(
     """
     <style>
+    .css-18e3th9 {
+        padding-top: 0rem;
+        padding-bottom: 10rem;
+        padding-left: 5rem;
+        padding-right: 5rem;
+    }
+    .css-1d391kg {
+        padding-top: 3.5rem;
+        padding-right: 1rem;
+        padding-bottom: 3.5rem;
+        padding-left: 1rem;
+    }
     .title {
         text-align: center;
         font-size: 6vw; /* Ajusta el tamaño del título para dispositivos móviles */
