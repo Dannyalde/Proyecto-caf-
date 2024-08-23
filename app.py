3aac26fb30409f38ca29a4e48447dd8d858c75cc
Danny <da.jimenezm@uniandes.edu.co>
2024-08-23 13:36:52 -0500

    pruebas formato imagen 1.2

--- app.py
+++ app.py
@@ -240,9 +240,9 @@ else:
         col1, col2 = st.columns(2)
 
         with col1:
-            st.image(img_normal.array, use_column_width=True)
+            st.image(Color.image, use_column_width=True)
             print("*"*20)
-            print(results.good_sample)
+            print(Color.image)
         #    #st.markdown("<p style='text-align: center; font-size: 18px; color: black; font-weight: bold; font-style: italic;'>Café bueno</p>", unsafe_allow_html=True)
 
         with col2:
